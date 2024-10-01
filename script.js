1c5f7d9ffd2ce458551e901e45d7f94cf3ad8f12
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2024-09-30 23:35:53 -0300

    new features

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 AOS.init();
+
+window.onload = function() {
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    myModal.show();
+}
+
+document.getElementById('btnNo').onclick = function() {
+    document.getElementById('errorMessage').style.display = 'block';
+    document.getElementById('contentModal').style.display = 'none';
+    document.querySelector('.modal-footer').style.display = 'none';
+    document.querySelector('.modal-header').style.display = 'none';
+}
